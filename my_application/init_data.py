3e9c64c2a0bc03036925eae629daba008cadd6c2
Andraz Brodnik <brodul@brodul.org>
2018-11-19 14:53:19 +0100

    Move init data

---
+++ my_application/init_data.py
@@ -0,0 +1,16 @@
+from my_application import db
+from my_application.models.user import User
+from my_application.models.pet import Pet
+
+
+def init_db():
+
+    db.create_all()
+
+    admin = User(username='admin', email='admin@example.com')
+    pet = Pet(name='Lara')
+
+    db.session.add(admin)
+    db.session.add(pet)
+
+    db.session.commit()
